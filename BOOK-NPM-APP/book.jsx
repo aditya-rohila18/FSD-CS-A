b73518ffc46c32faea0d1ddc5b87c5abf8e78313
Aditya Kumar Rohila <adityarohila6@gmail.com>
2025-10-29 19:47:11 +0530

    modified files

--- BOOK-NPM-APP/book.jsx
+++ BOOK-NPM-APP/book.jsx
@@ -2,6 +2,12 @@ import './App.css';
 
 function Book(){
     return(
-        <div></div>
-    )
+        <div className='card'>
+        <img src="https://m.media-amazon.com/images/I/31WIiECCtxL._AC_UF1000,1000_QL80_.jpg" width="200" height="200" alt="Book Image"/>
+        <h3>Title: Physics</h3>
+        <h4>Price: ₹385/-</h4>
+        <button>Add To Cart</button>        
+        </div>
+    );
 }
+export default Book;
--- BOOK-NPM-APP/index.jsx
+++ BOOK-NPM-APP/index.jsx
@@ -1,24 +1,19 @@
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import Book from "./book.jsx";
 
-function app(){
-    return(
-        <div>
-            <Book/>
-            <Book/>
-            <Book/>
-        </div>
-    );
+function App() {
+	return (
+		<div className="app-container">
+			<header>
+				<h1>Book Store</h1>
+			</header>
+			<main>
+				<Book />
+			</main>
+		</div>
+	);
 }
-function Book(){
-    return(
-        <div>
-            <img src="" width="200" height="200" alt="Book Image"/>
-            <h3>Title: Physics</h3>
-            <h4>Price: ₹385/-</h4>
-            <button>Add to Cart</button>
-        </div>
-    );
-}
-const parent = document.getElementById("root");
-const root = ReactDOM.createRoot(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
