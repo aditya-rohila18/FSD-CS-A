87acadf2113d9f873b3ae8bb841091311251bdf5
Aditya Kumar Rohila <adityarohila6@gmail.com>
2025-11-07 12:10:37 +0530

    added routes

--- REACT-ROUTER-DOM/client/src/App.jsx
+++ REACT-ROUTER-DOM/client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>}></Route>
+        <Route path="/" element={<h1>View Cart</h1>}></Route>
+        <Route path="/" element={<h1>View Order</h1>}></Route>
+        <Route path="/" element={<h1>Register Here</h1>}></Route>
+        <Route path="/" element={<h1>Login Here</h1>}></Route>
+        <Route path="/" element={<h1>404 Error Page</h1>}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
